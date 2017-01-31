542f860d17c1210c14989202b1c16899d9338e3f
Helge Hess <me@helgehess.eu>
2017-01-31 02:20:55 +0100

    Simple Swift configuration handler
    Leaks. But runs.

--- mods_baredemo/Sources/APIExtensions.swift
+++ mods_baredemo/Sources/APIExtensions.swift
@@ -259,3 +259,89 @@ extension module {
   }
   
 }
+
+
+// MARK: - Config Helpers
+
+protocol ApacheModuleConfig : class {
+
+  func merge(with new: Self) -> Self?
+  
+}
+
+extension ApacheModuleConfig {
+
+  static func merge(base: Self?, new: Self?) -> Self? {
+    // print("merge dir cfg: \(base) \(new)")
+    if base == nil { return new  }
+    if new  == nil { return base }
+    return base!.merge(with: new!)
+  }
+  
+  static func merge(_ pool: OpaquePointer?,
+                    _ base: UnsafeMutableRawPointer?,
+                    _ new:  UnsafeMutableRawPointer?)
+              -> UnsafeMutableRawPointer?
+  {
+    let baseCfg = Self.fromOpaque(base)
+    let newCfg  = Self.fromOpaque(new)
+    let merged  = Self.merge(base: baseCfg, new: newCfg)
+    // TODO: dealloc in pool
+    return merged?.passRetained()
+  }
+  
+  static func fromOpaque(_ ptr : UnsafeMutableRawPointer?) -> Self? {
+    guard let ptr = ptr else { return nil }
+    return Unmanaged<Self>.fromOpaque(ptr).takeUnretainedValue()
+  }
+  
+  func passRetained() -> UnsafeMutableRawPointer {
+    return Unmanaged.passRetained(self).toOpaque()
+  }
+  
+}
+
+final class ApacheDictionaryConfig
+            : ApacheModuleConfig, CustomStringConvertible
+{
+  
+  var values = [ String : String ]()
+  
+  func merge(with new: ApacheDictionaryConfig) -> ApacheDictionaryConfig? {
+    let merged = ApacheDictionaryConfig()
+    merged.values = values
+    for (k, v) in new.values {
+      merged.values.updateValue(v, forKey: k)
+    }
+    // print("merge \(self) + \(new) dir cfg: \(merged)")
+    return merged
+  }
+  
+  subscript(_ key : String) -> String? {
+    get { return values[key] }
+    set { values[key] = newValue }
+  }
+  
+  
+  // MARK: - Logging
+  
+  var description : String {
+    return "<ApacheConfig: \(values)>"
+  }
+
+
+  // MARK: - Callbacks
+  
+  static func create(_ pool: OpaquePointer?,
+                     _ dirname: UnsafeMutablePointer<Int8>?)
+              -> UnsafeMutableRawPointer?
+  {
+    let path = dirname != nil ? String(cString: dirname!) : nil
+    
+    let cfg = ApacheDictionaryConfig()
+    // print("create dir cfg, path: \(path ?? "-")")
+    // TODO: dealloc in pool
+    return cfg.passRetained()
+  }
+}
+
