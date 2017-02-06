ab998accfbd05d328deb87a1ba8136be2cc4a7de
Helge Hess <devteam@zeezide.de>
2017-02-06 23:37:27 +0100

    Add a Swift variant of APR_RETRIEVE_OPTIONAL_FN
    This is a macro in C and can automagically assign
    the proper function type. In Swift we need to
    specify the type, but can use this generic func
    to make lookup a little easier.

--- mods_baredemo/Sources/APIExtensions.swift
+++ mods_baredemo/Sources/APIExtensions.swift
@@ -341,8 +341,24 @@ final class ApacheDictionaryConfig
     
     let cfg = ApacheDictionaryConfig()
     // print("create dir cfg, path: \(path ?? "-")")
+    
     // TODO: dealloc in pool
+    
     return cfg.passRetained()
   }
 }
 
+
+// MARK: - Optional Functions
+
+// DBD test
+// Apache optional functions have the prefix `apr_OFN_`, e.g.
+// `apr_OFN_ap_dbd_acquire_t`.
+// Note: func types with UnsafeMutablePointer<request_rec>? coredump swiftc,
+//       e.g.: apr_OFN_ap_dbd_acquire_t
+
+// our funcs need to declare the type, there is no automagic macro way ...
+func APR_RETRIEVE_OPTIONAL_FN<T>(_ name: String) -> T? {
+  guard let fn = apr_dynamic_fn_retrieve(name) else { return nil }
+  return unsafeBitCast(fn, to: T.self) // TBD: is there a better way?
+}
